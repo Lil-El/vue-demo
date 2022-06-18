9127e2c2a82bd696dc00523d6189ce38557ab516
Lil-El <yxd99324@qq.com>
2022-06-18 15:22:33 +0800

    vue3 setup

---
+++ src/case/Setup/define.vue
@@ -0,0 +1,13 @@
+
+<script>
+import { defineComponent, mergeProps, h, defineProps } from "@vue/runtime-core";
+export default defineComponent({
+    props: ["name"],
+    setup(props) {
+        return mergeProps(props, {age: 12})
+    },
+    render() {
+        return h("div", {name: "Kpop"}, [ h("span", this.name), "-", h("span", this.age)])
+    }
+});
+</script>
---
+++ src/case/Setup/scriptSetup.vue
@@ -0,0 +1,9 @@
+<template>
+    <span>{{data.name}}</span>
+</template>
+
+<script setup> // 配合template使用
+import { mergeProps, defineProps } from "@vue/runtime-core";
+const prop = defineProps({ name: String });
+const data = mergeProps(prop, { age: 4 });
+</script>
