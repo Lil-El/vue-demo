31941133a84537a0cc00663a27a0f2078222b810
Lil-El <yxd99324@qq.com>
2023-04-13 10:37:17 +0800

    test script execution sequence

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <template>
-    <!-- <div @click="handleClick"> -->
-        <Watch name="setUp" />
-    <!-- </div> -->
+  <!-- <div @click="handleClick"> -->
+  <Watch name="setUp" />
+  <!-- </div> -->
+  <Life />
 </template>
 
 <script>
 import { reactive } from "@vue/reactivity";
 import Watch from "@/case/Computed/index.vue";
 import { watch } from "vue";
+import Life from "@/case/Life";
 export default {
-    components: { Watch },
-    setup() {
-        let state = reactive({
-            obj: 1,
-        });
-        watch(
-            () => state.obj,
-            () => {
-                console.log("change");
-            }
-        );
-        let handleClick = () => {
-            state.obj = "yxd";
-            state.obj = "mino";
-        };
-        return {
-            handleClick,
-        };
-    },
+  components: { Watch, Life },
+  setup() {
+    let state = reactive({
+      obj: 1,
+    });
+    watch(
+      () => state.obj,
+      () => {
+        console.log("change");
+      }
+    );
+    let handleClick = () => {
+      state.obj = "yxd";
+      state.obj = "mino";
+    };
+    return {
+      handleClick,
+    };
+  },
 };
 </script>
 
 <style lang="scss">
 * {
-    padding: 0;
-    margin: 0;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/case/Life/index.vue
@@ -0,0 +1,19 @@
+<template>
+  生命周期
+</template>
+
+<script setup>
+import { nextTick, onMounted, ref } from "vue";
+
+const keyword = ref();
+
+onMounted(() => {
+  keyword.value = "Mino";
+  console.log(keyword);
+});
+
+console.log(keyword.value); // undefined: 执行早于onMounted
+nextTick(()=>{
+  console.log(keyword.value);
+})
+</script>
